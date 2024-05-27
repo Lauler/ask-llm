92d557475f6d3280235ab1a8ebdbb45220746bf9
Lauler <fatonr@gmail.com>
2024-05-27 09:08:13 +0200

    container and correlation matrix

--- askllm_hf.py
+++ askllm_hf.py
@@ -1,10 +1,11 @@
 import torch
+import pandas as pd
 from torch.nn.functional import softmax
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset, Dataset
 from functools import partial
 from tqdm import tqdm
-from src.ask_llm.prompt import apply_prompt_template
+from ask_llm.prompt import apply_prompt_template
 
 device = "cuda"  # the device to load the model onto
 
@@ -14,12 +15,14 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
     attn_implementation="flash_attention_2",
 )
+model = torch.compile(model)
 
 tokenizer = AutoTokenizer.from_pretrained(
     "mistralai/Mistral-7B-Instruct-v0.2", padding_side="left"
 )
 
 tokenizer.pad_token = tokenizer.eos_token
+model.config.pad_token_id = model.config.eos_token_id
 
 # Load the dataset in streaming mode
 dataset = load_dataset(
@@ -35,10 +38,15 @@ ds = dataset["train"].take(2000)
 
 # Convert iterabledataset to dataset
 ds = Dataset.from_generator(partial(gen_from_iterable_dataset, ds), features=ds.features)
-ds = ds.map(apply_prompt_template, batched=False, fn_kwargs={"tokenizer": tokenizer})
+ds = ds.map(
+    apply_prompt_template, batched=False, fn_kwargs={"tokenizer": tokenizer, "max_length": 450}
+)
 
 # Turn ds into a dataloader
-dataloader = torch.utils.data.DataLoader(ds, batch_size=4)
+dataloader = torch.utils.data.DataLoader(ds, batch_size=4, num_workers=3)
+
+probs_450 = []
+text_dicts = []
 
 # Generate with dataloader
 for batch in tqdm(dataloader):
@@ -57,15 +65,39 @@ for batch in tqdm(dataloader):
         max_new_tokens=1,
         do_sample=False,
         num_beams=1,
-        output_scores=True,
+        output_logits=True,
         return_dict_in_generate=True,
     )
-    for logits in generated_ids["scores"]:
+    for i, logits in enumerate(generated_ids["logits"][0]):
         decoded = tokenizer.decode(logits.argmax(-1))
         softmax_score = softmax(logits, dim=-1)
-        print(
-            (
-                f"{decoded} \n"
-                f"Yes probs: {softmax_score[:, tokenizer.convert_tokens_to_ids('▁Yes')]}\n"
-            )
+        probs_450.append(softmax_score[tokenizer.convert_tokens_to_ids("▁Yes")].to("cpu").item())
+        prob = softmax_score[tokenizer.convert_tokens_to_ids("▁Yes")].to("cpu").item()
+        text_dicts.append(
+            {
+                "text": batch["text"][i],
+                "url": batch["url"][i],
+                "timestamp": batch["timestamp"][i],
+                "prob": prob,
+            }
         )
+
+df = pd.DataFrame(text_dicts)
+df.sort_values("prob", ascending=False)[1126:1127].values
+
+# Flatten the list of tensors
+probs_450 = torch.cat(probs_450).tolist()
+
+
+# Find index with minimum prob
+min_prob_index = probs_450.index(min(probs_450))
+argmin_prob_index = torch.argmin(torch.tensor(probs_450))
+
+df = pd.DataFrame(
+    {
+        "probs_450": probs_450[0:192],
+    }
+)
+
+# Correlation matrix
+df.corr()
